4dcdd5e3864ac780a50277c95c62c2908a979983
LAPTOP-LU820F0G\Keita_Sugitani <tfue888888@gmail.com>
2026-08-19 21:31:02 +0900

    Render deployment preparation

--- server.js
+++ server.js
@@ -879,8 +879,10 @@ app.put("/products/:id", (req, res) => {
 });
 
 if(require.main === module ){
-    app.listen(3000,() => {
-        console.log("サーバー起動:http://localhost:3000");
+    const PORT = process.env.PORT || 3000;
+
+    app.listen(PORT, "0.0.0.0",() => {
+        console.log(`サーバー起動:${PORT}`);
     });
 }
 
